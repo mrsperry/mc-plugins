f16bebe3e387b53cb884de0206c1d259d88bddfd
mrsperry <mrjoshuasperry@gmail.com>
2024-08-03 19:56:15 -0400

    Fix up auto-stack

--- auto-stack/src/main/java/com/mrjoshuasperry/autostack/Main.java
+++ auto-stack/src/main/java/com/mrjoshuasperry/autostack/Main.java
@@ -1,17 +1,16 @@
 package com.mrjoshuasperry.autostack;
 
-import com.mrjoshuasperry.mcutils.types.ToolTypes;
-import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.mrjoshuasperry.mcutils.types.ToolTypes;
 
 public class Main extends JavaPlugin implements Listener {
     @Override
@@ -19,60 +18,44 @@ public class Main extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
     }
 
-    private void searchForStack(Player player, ItemStack item) {
-        // Only search for stacks if the item would be removed
-        if (player.getGameMode() != GameMode.SURVIVAL && player.getGameMode() != GameMode.ADVENTURE) {
-            return;
-        }
-
-        // Get the player's inventory
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
         PlayerInventory inventory = player.getInventory();
+        ItemStack item = event.getItemInHand();
+        EquipmentSlot hand = event.getHand();
 
-        // Don't auto stack tools
-        if (ToolTypes.getAllToolTypes().contains((item.getType()))) {
+        if (player.getGameMode() != GameMode.SURVIVAL) {
             return;
         }
 
-        // Only auto stack when amount would equal zero
+        // Only stack if the amount will equal zero after placing
         if (item.getAmount() != 1) {
             return;
         }
 
-        int handSlot = inventory.getHeldItemSlot();
-        for (int index = 0; index < 36; index++) {
-            if (index == handSlot) {
-                continue;
-            }
-
-            // Get the current slot
-            ItemStack current = inventory.getItem(index);
-            // Skip over empty slots
-            if (current == null) {
-                continue;
-            }
-
-            // Guarantee items are the same type
-            if (!current.isSimilar(item)) {
-                continue;
-            }
-
-            inventory.setItemInMainHand(current);
-            current.setAmount(0);
+        // Disallow stacking of tools
+        if (ToolTypes.getAllToolTypes().contains((item.getType()))) {
             return;
         }
-    }
 
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        if (event.getHand() == EquipmentSlot.HAND) {
-            this.searchForStack(event.getPlayer(), event.getItemInHand());
+        for (ItemStack stack : inventory.getContents()) {
+            if (stack == item) {
+                continue;
+            }
+
+            if (stack.getType() != item.getType()) {
+                continue;
+            }
+
+            if (hand == EquipmentSlot.HAND) {
+                inventory.setItemInMainHand(stack.clone());
+            } else {
+                inventory.setItemInOffHand(stack.clone());
+            }
+
+            stack.setAmount(0);
+            break;
         }
-    }
-
-    @EventHandler
-    public void onPlayerDropItem(PlayerDropItemEvent event) {
-        Player player = event.getPlayer();
-        this.searchForStack(player, event.getItemDrop().getItemStack());
-        player.updateInventory();
     }
 }
