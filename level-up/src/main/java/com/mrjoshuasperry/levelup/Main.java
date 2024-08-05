728b141fab5228d96ce712358ecb741b709f389f
mrsperry <mrjoshuasperry@gmail.com>
2024-08-04 20:31:42 -0400

    Fix up level-up

--- level-up/src/main/java/com/mrjoshuasperry/levelup/Main.java
+++ level-up/src/main/java/com/mrjoshuasperry/levelup/Main.java
@@ -1,11 +1,18 @@
 package com.mrjoshuasperry.levelup;
 
-import com.google.common.collect.Lists;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
@@ -13,125 +20,106 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Random;
+import com.google.common.collect.Lists;
 
 public class Main extends JavaPlugin implements Listener {
-    private final HashSet<Integer> levels = new HashSet<>();
-    private final HashSet<Color> colors = new HashSet<>();
+    private final Set<Integer> levels = new HashSet<>();
+    private final Set<Color> colors = new HashSet<>();
     private final Random random = new Random();
+    private final NamespacedKey levelUpKey = new NamespacedKey(this, "level-up");
 
-    private final ArrayList<FireworkEffect.Type> starTypes = Lists.newArrayList(
-        FireworkEffect.Type.BALL,
-        FireworkEffect.Type.BALL_LARGE,
-        FireworkEffect.Type.BURST,
-        FireworkEffect.Type.STAR
-    );
+    private final List<FireworkEffect.Type> starTypes = Lists.newArrayList(
+            FireworkEffect.Type.BALL,
+            FireworkEffect.Type.BALL_LARGE,
+            FireworkEffect.Type.BURST,
+            FireworkEffect.Type.STAR);
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(this, this);
 
-        if (this.getConfig().isSet("levels")) {
-            for (String level : this.getConfig().getStringList("levels")) {
-                try {
-                    this.levels.add(Integer.parseInt(level));
-                } catch (NumberFormatException ex) {
-                    this.getLogger().severe("Invalid number format: " + level);
-                }
+        FileConfiguration config = this.getConfig();
+        for (String level : config.getStringList("levels")) {
+            try {
+                this.levels.add(Integer.parseInt(level));
+            } catch (NumberFormatException ex) {
+                this.getLogger().severe("Invalid number format for level: " + level);
             }
         }
-        if (this.getConfig().isSet("colors")) {
-            for (String color : this.getConfig().getStringList("colors")) {
-                this.colors.add(getColor(color));
+
+        for (Object color : config.getList("colors")) {
+            if (!(color instanceof Map)) {
+                continue;
+            }
+
+            Map<?, ?> colorMap = (Map<?, ?>) color;
+            try {
+                int alpha = Integer.parseInt((String) colorMap.get("alpha"));
+                int red = Integer.parseInt((String) colorMap.get("red"));
+                int green = Integer.parseInt((String) colorMap.get("green"));
+                int blue = Integer.parseInt((String) colorMap.get("blue"));
+
+                this.colors.add(Color.fromARGB(alpha, red, green, blue));
+            } catch (ClassCastException ex) {
+                this.getLogger().severe("Invalid color format: " + color);
             }
         }
     }
 
     @EventHandler
     public void onPlayerLevelChange(PlayerLevelChangeEvent event) {
-        if (this.levels.contains(event.getNewLevel())) {
-            if (event.getOldLevel() < event.getNewLevel()) {
-                if (!this.colors.isEmpty()) {
-                    Location location = event.getPlayer().getLocation();
-                    World world = location.getWorld();
-                    if (world == null) {
-                        return;
-                    }
-
-                    Firework firework = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
-                    FireworkMeta meta = firework.getFireworkMeta();
-                    meta.setPower(1);
-                    meta.addEffects(FireworkEffect.builder()
-                            .with(this.starTypes.get(this.random.nextInt(this.starTypes.size())))
-                            .flicker(true)
-                            .trail(true)
-                            .withColor(getColorFromSet())
-                            .withFade(getColorFromSet())
-                            .build());
-                    firework.setFireworkMeta(meta);
-                }
-            }
+        if (!this.levels.contains(event.getNewLevel())) {
+            return;
         }
+
+        // Don't send a firework when spending levels
+        if (event.getOldLevel() > event.getNewLevel()) {
+            return;
+        }
+
+        if (this.colors.isEmpty()) {
+            return;
+        }
+
+        Location location = event.getPlayer().getLocation();
+        World world = location.getWorld();
+
+        Firework firework = (Firework) world.spawnEntity(location, EntityType.FIREWORK_ROCKET);
+        FireworkMeta meta = firework.getFireworkMeta();
+        meta.setPower(1);
+        meta.addEffects(FireworkEffect.builder()
+                .with(this.starTypes.get(this.random.nextInt(this.starTypes.size())))
+                .flicker(true)
+                .trail(true)
+                .withColor(getRandomColor())
+                .withFade(getRandomColor())
+                .build());
+        firework.setFireworkMeta(meta);
+
+        firework.getPersistentDataContainer().set(this.levelUpKey, PersistentDataType.BYTE,
+                (byte) 1);
     }
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (event.getDamager().getType() == EntityType.FIREWORK) {
-            event.setCancelled(true);
+        Entity entity = event.getDamager();
+
+        if (entity.getType() != EntityType.FIREWORK_ROCKET) {
+            return;
         }
+
+        if (!entity.getPersistentDataContainer().has(this.levelUpKey, PersistentDataType.BYTE)) {
+            return;
+        }
+
+        event.setCancelled(true);
     }
 
-    private Color getColorFromSet() {
+    private Color getRandomColor() {
         return (Color) this.colors.toArray()[this.random.nextInt(this.colors.size())];
-    }
-
-    private Color getColor(String color) {
-        try {
-            if (color.equalsIgnoreCase("aqua")) {
-                return Color.AQUA;
-            } else if (color.equalsIgnoreCase("black")) {
-                return Color.BLACK;
-            } else if (color.equalsIgnoreCase("blue")) {
-                return Color.BLUE;
-            } else if (color.equalsIgnoreCase("fuchsia")) {
-                return Color.FUCHSIA;
-            } else if (color.equalsIgnoreCase("gray")) {
-                return Color.GRAY;
-            } else if (color.equalsIgnoreCase("green")) {
-                return Color.GREEN;
-            } else if (color.equalsIgnoreCase("lime")) {
-                return Color.LIME;
-            } else if (color.equalsIgnoreCase("maroon")) {
-                return Color.MAROON;
-            } else if (color.equalsIgnoreCase("navy")) {
-                return Color.NAVY;
-            } else if (color.equalsIgnoreCase("olive")) {
-                return Color.OLIVE;
-            } else if (color.equalsIgnoreCase("orange")) {
-                return Color.ORANGE;
-            } else if (color.equalsIgnoreCase("purple")) {
-                return Color.PURPLE;
-            } else if (color.equalsIgnoreCase("red")) {
-                return Color.RED;
-            } else if (color.equalsIgnoreCase("silver")) {
-                return Color.SILVER;
-            } else if (color.equalsIgnoreCase("teal")) {
-                return Color.TEAL;
-            } else if (color.equalsIgnoreCase("white")) {
-                return Color.WHITE;
-            } else if (color.equalsIgnoreCase("yellow")) {
-                return Color.YELLOW;
-            } else {
-                throw new Exception();
-            }
-        } catch (Exception ex) {
-            this.getLogger().severe("Invalid color name: " + color);
-        }
-        return Color.WHITE;
     }
 }
