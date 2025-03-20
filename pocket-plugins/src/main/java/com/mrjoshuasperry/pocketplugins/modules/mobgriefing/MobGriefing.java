d2f73e3e680644de184a56aec08f24523be2c53d
mrsperry <mrjoshuasperry@gmail.com>
2025-03-20 17:22:43 -0400

    Combine sheep and enderman griefing protection into one module

---
+++ pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/mobgriefing/MobGriefing.java
@@ -0,0 +1,49 @@
+package com.mrjoshuasperry.pocketplugins.modules.mobgriefing;
+
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+
+import com.mrjoshuasperry.pocketplugins.utils.Module;
+
+/** @author mrsperry */
+public class MobGriefing extends Module {
+  protected boolean noSheepGriefing;
+  protected boolean noEndermanGriefing;
+
+  public MobGriefing() {
+    super("MobGriefing");
+  }
+
+  @Override
+  public void initialize(ConfigurationSection readableConfig, ConfigurationSection writableConfig) {
+    super.initialize(readableConfig, writableConfig);
+
+    this.noSheepGriefing = readableConfig.getBoolean("no-sheep-griefing", true);
+    this.noEndermanGriefing = readableConfig.getBoolean("no-enderman-griefing", true);
+  }
+
+  @EventHandler
+  public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+    Entity entity = event.getEntity();
+    if (entity == null) {
+      return;
+    }
+
+    EntityType type = entity.getType();
+
+    if (this.noSheepGriefing && type == EntityType.SHEEP) {
+      entity.getWorld().playSound(entity.getLocation(), Sound.BLOCK_GRASS_PLACE, 1, 1);
+      event.setCancelled(true);
+      return;
+    }
+
+    if (this.noEndermanGriefing && type == EntityType.ENDERMAN) {
+      event.setCancelled(true);
+      return;
+    }
+  }
+}
