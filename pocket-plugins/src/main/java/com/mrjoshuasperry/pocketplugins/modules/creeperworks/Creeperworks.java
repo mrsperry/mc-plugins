2b0eeb0381e821da333ec8373c03dde1e906346f
mrsperry <mrjoshuasperry@gmail.com>
2025-03-22 18:20:11 -0400

    Add creeperworks as a module

---
+++ pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/creeperworks/Creeperworks.java
@@ -0,0 +1,34 @@
+package com.mrjoshuasperry.pocketplugins.modules.creeperworks;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+import com.mrjoshuasperry.pocketplugins.utils.Module;
+
+public class Creeperworks extends Module {
+  public Creeperworks() {
+    super("Creeperworks");
+  }
+
+  @EventHandler
+  public void onEntityExplode(EntityExplodeEvent event) {
+    Entity entity = event.getEntity();
+    if (!(entity instanceof Creeper)) {
+      return;
+    }
+
+    Firework firework = (Firework) entity.getWorld().spawnEntity(entity.getLocation(), EntityType.FIREWORK_ROCKET);
+    FireworkMeta meta = firework.getFireworkMeta();
+    meta.setPower(this.getPlugin().getRandom().nextInt(1, 3));
+    meta.addEffect(FireworkEffect.builder().withColor(Color.LIME).with(Type.CREEPER).build());
+    firework.setFireworkMeta(meta);
+  }
+}
