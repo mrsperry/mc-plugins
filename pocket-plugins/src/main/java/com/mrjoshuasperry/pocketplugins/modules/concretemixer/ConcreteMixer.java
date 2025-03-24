e568312d8f7a908d3ccbedc18378990fb9375fbc
mrsperry <mrjoshuasperry@gmail.com>
2025-03-24 04:37:05 -0400

    Rewrite concrete mixer

--- pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/concretemixer/ConcreteMixer.java
+++ pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/concretemixer/ConcreteMixer.java
@@ -1,93 +1,93 @@
 package com.mrjoshuasperry.pocketplugins.modules.concretemixer;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Levelled;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 
 import com.mrjoshuasperry.pocketplugins.utils.Module;
 
-/** @author TimPCunningham */
+/** @author Idea by TimPCunningham, implemented by mrsperry */
 public class ConcreteMixer extends Module {
-    private final List<Material> concrete = List.of(
-            Material.BLACK_CONCRETE_POWDER,
-            Material.CYAN_CONCRETE_POWDER,
-            Material.BLUE_CONCRETE_POWDER,
-            Material.BROWN_CONCRETE_POWDER,
-            Material.GRAY_CONCRETE_POWDER,
-            Material.GREEN_CONCRETE_POWDER,
-            Material.LIGHT_BLUE_CONCRETE_POWDER,
-            Material.LIGHT_GRAY_CONCRETE_POWDER,
-            Material.LIME_CONCRETE_POWDER,
-            Material.MAGENTA_CONCRETE_POWDER,
-            Material.ORANGE_CONCRETE_POWDER,
-            Material.PINK_CONCRETE_POWDER,
-            Material.PURPLE_CONCRETE_POWDER,
-            Material.RED_CONCRETE_POWDER,
-            Material.WHITE_CONCRETE_POWDER,
-            Material.YELLOW_CONCRETE_POWDER);
+    protected Map<Material, Material> concrete = new HashMap<>();
+    {
+        concrete.put(Material.BLACK_CONCRETE_POWDER, Material.BLACK_CONCRETE);
+        concrete.put(Material.CYAN_CONCRETE_POWDER, Material.CYAN_CONCRETE);
+        concrete.put(Material.BLUE_CONCRETE_POWDER, Material.BLUE_CONCRETE);
+        concrete.put(Material.BROWN_CONCRETE_POWDER, Material.BROWN_CONCRETE);
+        concrete.put(Material.GRAY_CONCRETE_POWDER, Material.GRAY_CONCRETE);
+        concrete.put(Material.GREEN_CONCRETE_POWDER, Material.GREEN_CONCRETE);
+        concrete.put(Material.LIGHT_BLUE_CONCRETE_POWDER, Material.LIGHT_BLUE_CONCRETE);
+        concrete.put(Material.LIGHT_GRAY_CONCRETE_POWDER, Material.LIGHT_GRAY_CONCRETE);
+        concrete.put(Material.LIME_CONCRETE_POWDER, Material.LIME_CONCRETE);
+        concrete.put(Material.MAGENTA_CONCRETE_POWDER, Material.MAGENTA_CONCRETE);
+        concrete.put(Material.ORANGE_CONCRETE_POWDER, Material.ORANGE_CONCRETE);
+        concrete.put(Material.PINK_CONCRETE_POWDER, Material.PINK_CONCRETE);
+        concrete.put(Material.PURPLE_CONCRETE_POWDER, Material.PURPLE_CONCRETE);
+        concrete.put(Material.RED_CONCRETE_POWDER, Material.RED_CONCRETE);
+        concrete.put(Material.WHITE_CONCRETE_POWDER, Material.WHITE_CONCRETE);
+        concrete.put(Material.YELLOW_CONCRETE_POWDER, Material.YELLOW_CONCRETE);
+    }
 
-    private int waterUseChance;
+    protected int waterUseChance;
 
     public ConcreteMixer(ConfigurationSection readableConfig, ConfigurationSection writableConfig) {
         super(readableConfig, writableConfig);
 
         this.waterUseChance = readableConfig.getInt("water-use-chance", 5);
+
+        Bukkit.getScheduler().runTaskTimer(this.getPlugin(), this::convertConcretePowder, 0, 5);
     }
 
-    private void handleConcreteHarden(Player player, ItemStack item, Block block) {
-        Material type = block.getType();
-        Levelled levelled = (Levelled) block.getBlockData();
-        Material result = Material.valueOf(type.name().substring(0, type.name().length() - 7));
-        block.getWorld().dropItem(block.getLocation().add(0.5, 1.5, 0.5), new ItemStack(result));
+    protected void convertConcretePowder() {
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntitiesByClass(Item.class)) {
+                ItemStack item = ((Item) entity).getItemStack();
+                if (item == null) {
+                    continue;
+                }
 
-        if (item.getAmount() == 1) {
-            player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-        } else {
-            item.setAmount(item.getAmount() - 1);
+                Material itemType = item.getType();
+                if (!this.concrete.keySet().contains(itemType)) {
+                    continue;
+                }
+
+                Block block = entity.getLocation().getBlock();
+                if (block.getType() != Material.WATER_CAULDRON) {
+                    continue;
+                }
+
+                Levelled levelled = (Levelled) block.getBlockData();
+                if (levelled.getLevel() == 0) {
+                    continue;
+                }
+
+                block.getWorld().dropItem(block.getLocation().add(0.5, 1.5, 0.5),
+                        new ItemStack(this.concrete.get(itemType)));
+                item.subtract();
+
+                if (this.getPlugin().getRandom().nextInt(100) > this.waterUseChance) {
+                    continue;
+                }
+
+                int newLevel = levelled.getLevel() - 1;
+
+                if (newLevel == 0) {
+                    block.setType(Material.CAULDRON);
+                    continue;
+                }
+
+                levelled.setLevel(levelled.getLevel() - 1);
+                block.setBlockData(levelled);
+            }
         }
-
-        if (this.getPlugin().getRandom().nextInt(100) <= this.waterUseChance) {
-            levelled.setLevel(levelled.getLevel() - 1);
-            block.setBlockData(levelled);
-        }
-    }
-
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getHand() == EquipmentSlot.OFF_HAND) {
-            return;
-        }
-
-        ItemStack item = event.getItem();
-        Block block = event.getClickedBlock();
-
-        if (block == null) {
-            return;
-        }
-
-        if (block.getType() != Material.CAULDRON) {
-            return;
-        }
-
-        Levelled levelled = (Levelled) block.getBlockData();
-        if (levelled.getLevel() == 0) {
-            return;
-        }
-
-        Material type = item.getType();
-        if (!concrete.contains(type)) {
-            return;
-        }
-
-        event.setCancelled(true);
-        this.handleConcreteHarden(event.getPlayer(), item, block);
     }
 }
