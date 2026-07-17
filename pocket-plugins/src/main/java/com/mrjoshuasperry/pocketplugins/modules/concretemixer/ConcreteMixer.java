9e5b1b381eccfb23fbeb00d89e147cdd18a41a48
Josh Sperry <mrjoshuasperry@gmail.com>
2026-07-17 01:57:37 -0400

    Track concrete powder by event instead of scanning the world (#19)

--- pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/concretemixer/ConcreteMixer.java
+++ pocket-plugins/src/main/java/com/mrjoshuasperry/pocketplugins/modules/concretemixer/ConcreteMixer.java
@@ -1,16 +1,21 @@
 package com.mrjoshuasperry.pocketplugins.modules.concretemixer;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Levelled;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.world.EntitiesLoadEvent;
 import org.bukkit.inventory.ItemStack;
 
 import com.mrjoshuasperry.pocketplugins.utils.Module;
@@ -39,6 +44,13 @@ public class ConcreteMixer extends Module {
 
     protected int waterUseChance;
 
+    /**
+     * Only powder that is actually loose in the world, so the timer below never has
+     * to look at unrelated items. Entries are dropped once the entity stops being
+     * valid, which covers pickup, despawn, death and chunk unload.
+     */
+    protected final Set<Item> trackedPowder = new HashSet<>();
+
     public ConcreteMixer(ConfigurationSection readableConfig, ConfigurationSection writableConfig) {
         super(readableConfig, writableConfig);
 
@@ -47,47 +59,79 @@ public class ConcreteMixer extends Module {
         Bukkit.getScheduler().runTaskTimer(this.getPlugin(), this::convertConcretePowder, 0, 5);
     }
 
-    protected void convertConcretePowder() {
-        for (World world : Bukkit.getWorlds()) {
-            for (Entity entity : world.getEntitiesByClass(Item.class)) {
-                ItemStack item = ((Item) entity).getItemStack();
-                if (item == null) {
-                    continue;
-                }
+    @EventHandler
+    public void trackSpawnedPowder(ItemSpawnEvent event) {
+        Item entity = event.getEntity();
 
-                Material itemType = item.getType();
-                if (!this.concrete.keySet().contains(itemType)) {
-                    continue;
-                }
+        if (this.concrete.containsKey(entity.getItemStack().getType())) {
+            this.trackedPowder.add(entity);
+        }
+    }
 
-                Block block = entity.getLocation().getBlock();
-                if (block.getType() != Material.WATER_CAULDRON) {
-                    continue;
-                }
-
-                Levelled levelled = (Levelled) block.getBlockData();
-                if (levelled.getLevel() == 0) {
-                    continue;
-                }
-
-                block.getWorld().dropItem(block.getLocation().add(0.5, 1.5, 0.5),
-                        new ItemStack(this.concrete.get(itemType)));
-                item.subtract();
-
-                if (this.getPlugin().getRandom().nextInt(100) > this.waterUseChance) {
-                    continue;
-                }
-
-                int newLevel = levelled.getLevel() - 1;
-
-                if (newLevel == 0) {
-                    block.setType(Material.CAULDRON);
-                    continue;
-                }
-
-                levelled.setLevel(levelled.getLevel() - 1);
-                block.setBlockData(levelled);
+    /**
+     * Powder already lying on the ground does not fire {@link ItemSpawnEvent} when
+     * its chunk comes back, so anything dropped before a restart or a chunk unload
+     * has to be picked back up here.
+     */
+    @EventHandler
+    public void trackLoadedPowder(EntitiesLoadEvent event) {
+        for (Entity entity : event.getEntities()) {
+            if (!(entity instanceof Item)) {
+                continue;
             }
+
+            Item item = (Item) entity;
+            if (this.concrete.containsKey(item.getItemStack().getType())) {
+                this.trackedPowder.add(item);
+            }
+        }
+    }
+
+    protected void convertConcretePowder() {
+        Iterator<Item> iterator = this.trackedPowder.iterator();
+
+        while (iterator.hasNext()) {
+            Item entity = iterator.next();
+
+            if (!entity.isValid()) {
+                iterator.remove();
+                continue;
+            }
+
+            ItemStack item = entity.getItemStack();
+            // Also covers the stack having been emptied by an earlier conversion
+            Material converted = this.concrete.get(item.getType());
+            if (converted == null) {
+                iterator.remove();
+                continue;
+            }
+
+            Block block = entity.getLocation().getBlock();
+            if (block.getType() != Material.WATER_CAULDRON) {
+                continue;
+            }
+
+            Levelled levelled = (Levelled) block.getBlockData();
+            if (levelled.getLevel() == 0) {
+                continue;
+            }
+
+            block.getWorld().dropItem(block.getLocation().add(0.5, 1.5, 0.5), new ItemStack(converted));
+            item.subtract();
+
+            if (this.getPlugin().getRandom().nextInt(100) > this.waterUseChance) {
+                continue;
+            }
+
+            int newLevel = levelled.getLevel() - 1;
+
+            if (newLevel == 0) {
+                block.setType(Material.CAULDRON);
+                continue;
+            }
+
+            levelled.setLevel(levelled.getLevel() - 1);
+            block.setBlockData(levelled);
         }
     }
 }
